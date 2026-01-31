5a76d4a205a859368a500c578794bc28094a70fd
Raphael Faber <raphael.f@live.com>
2026-01-31 15:13:29 -0300

    Change project output type to Library and update sample usage code; remove unnecessary compiled files

--- ExcelToSql/samples/SampleUsage.cs
+++ ExcelToSql/samples/SampleUsage.cs
@@ -1,6 +1,15 @@
-﻿using ExcelToSql.Extractors;
+﻿// Sample usage (kept for reference). This file is not compiled into the library.
+// To run the sample, copy the code into a console app or enable compilation temporarily.
+
+using ExcelToSql.Extractors;
 using ExcelToSql.Models;
 
-string filePath = "/home/raphaelfaber/Documentos/Respositories/exceltosql/ExcelToSql/sample.xlsx";
-TableExtractor extractor = TableExtractorFactory.CreateExtractor(filePath);
-Table table = extractor.ExtractTable(1);
+public static class SampleUsage
+{
+    public static ExcelToSql.Models.Table LoadSample()
+    {
+        string filePath = "samples/sample.xlsx";
+        TableExtractor extractor = TableExtractorFactory.CreateExtractor(filePath);
+        return extractor.ExtractTable(1);
+    }
+}
